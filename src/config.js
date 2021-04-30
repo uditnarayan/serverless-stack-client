47b43caf3a9377574373b85aa4f4dcf0973cd241
Udit Narayan <info.udit90@gmail.com>
2021-04-30 18:14:51 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "ap-south-1",
+    BUCKET: "notes-app-sst-uploads",
+  },
+  apiGateway: {
+    REGION: "ap-south-1",
+    URL: "https://zqpu9o0l6k.execute-api.ap-south-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_FXZ9ljjMo",
+    APP_CLIENT_ID: "35s6ncrs102r63e54b6t6aejkt",
+    IDENTITY_POOL_ID: "ap-south-1:ce93f07d-9f1e-45fa-ab22-a3e32115bbf6",
+  },
+};
+
+export default config;
