d70d62af11c0378f1b9db50304e17b62b30d3df2
Udit Narayan <info.udit90@gmail.com>
2021-04-30 18:38:45 +0530

    login/logout workflow worksgit add .

---
+++ src/containers/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { Auth } from "aws-amplify";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useAppContext } from "../libs/contextLib";
+import "./Login.css";
+
+export default function Login() {
+  const { userHasAuthenticated } = useAppContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const history = useHistory();
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      await Auth.signIn(email, password);
+      userHasAuthenticated(true);
+      history.push("/");
+    } catch (e) {
+      alert(e.message);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group size="lg" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button block size="lg" type="submit" disabled={!validateForm()}>
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
+}
